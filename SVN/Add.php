64c40091541faf4afa6eb130cc20222478d538ed
Michiel Rook <mrook@php.net>
2011-08-07 14:04:31 +0000

    Formatting
    git-svn-id: http://svn.php.net/repository/pear/packages/VersionControl_SVN/trunk@314424 c90b9560-bf6c-de11-be94-00142212c4b1

--- SVN/Add.php
+++ SVN/Add.php
@@ -1,53 +1,55 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP version 5                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2004-2007, Clay Loveless                               |
-// | All rights reserved.                                                 |
-// +----------------------------------------------------------------------+
-// | This LICENSE is in the BSD license style.                            |
-// | http://www.opensource.org/licenses/bsd-license.php                   |
-// |                                                                      |
-// | Redistribution and use in source and binary forms, with or without   |
-// | modification, are permitted provided that the following conditions   |
-// | are met:                                                             |
-// |                                                                      |
-// |  * Redistributions of source code must retain the above copyright    |
-// |    notice, this list of conditions and the following disclaimer.     |
-// |                                                                      |
-// |  * Redistributions in binary form must reproduce the above           |
-// |    copyright notice, this list of conditions and the following       |
-// |    disclaimer in the documentation and/or other materials provided   |
-// |    with the distribution.                                            |
-// |                                                                      |
-// |  * Neither the name of Clay Loveless nor the names of contributors   |
-// |    may be used to endorse or promote products derived from this      |
-// |    software without specific prior written permission.               |
-// |                                                                      |
-// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
-// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
-// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
-// | FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE      |
-// | COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  |
-// | INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
-// | BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     |
-// | LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER     |
-// | CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT   |
-// | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN    |
-// | ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE      |
-// | POSSIBILITY OF SUCH DAMAGE.                                          |
-// +----------------------------------------------------------------------+
-// | Author: Clay Loveless <clay@killersoft.com>                          |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
 
-/**
- * @package     VersionControl_SVN
- * @category    VersionControl
- * @author      Clay Loveless <clay@killersoft.com>
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/** +----------------------------------------------------------------------+
+ * | PHP version 5                                                        |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2004-2007, Clay Loveless                               |
+ * | All rights reserved.                                                 |
+ * +----------------------------------------------------------------------+
+ * | This LICENSE is in the BSD license style.                            |
+ * | http: *www.opensource.org/licenses/bsd-license.php                   |
+ * |                                                                      |
+ * | Redistribution and use in source and binary forms, with or without   |
+ * | modification, are permitted provided that the following conditions   |
+ * | are met:                                                             |
+ * |                                                                      |
+ * |  * Redistributions of source code must retain the above copyright    |
+ * |    notice, this list of conditions and the following disclaimer.     |
+ * |                                                                      |
+ * |  * Redistributions in binary form must reproduce the above           |
+ * |    copyright notice, this list of conditions and the following       |
+ * |    disclaimer in the documentation and/or other materials provided   |
+ * |    with the distribution.                                            |
+ * |                                                                      |
+ * |  * Neither the name of Clay Loveless nor the names of contributors   |
+ * |    may be used to endorse or promote products derived from this      |
+ * |    software without specific prior written permission.               |
+ * |                                                                      |
+ * | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
+ * | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
+ * | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
+ * | FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE      |
+ * | COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  |
+ * | INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
+ * | BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     |
+ * | LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER     |
+ * | CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT   |
+ * | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN    |
+ * | ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE      |
+ * | POSSIBILITY OF SUCH DAMAGE.                                          |
+ * +----------------------------------------------------------------------+
+ * | Author: Clay Loveless <clay@killersoft.com>                          |
+ * +----------------------------------------------------------------------+
+ *
+ * @category VersionControl
+ * @package  VersionControl_SVN
+ * @author   Clay Loveless <clay@killersoft.com>
+ * @author   Michiel Rook <mrook@php.net>
+ * @license  http://www.killersoft.com/LICENSE.txt BSD License
+ * @version  SVN: $Id$
+ * @link     http://pear.php.net/package/VersionControl_SVN
  */
 
 /**
@@ -78,11 +80,11 @@
  *  'non-recursive' =>  true|false,
  *                      // operate on single directory only
  *  'auto-props'    =>  true|false,
- *                      // enable automatic properties. Setting to false IS NOT the same
- *                      // as setting no-auto-props to true.
+ *                      // enable automatic properties. Setting to false IS NOT
+ *                      // the same as setting no-auto-props to true.
  *  'no-auto-props' =>  true|false
- *                      // disable automatic properties. Setting to false IS NOT the same
- *                      // as setting auto-props to true.
+ *                      // disable automatic properties. Setting to false IS NOT
+ *                      // the same as setting auto-props to true.
  * );
  *
  * </code>
@@ -123,10 +125,13 @@
  *
  * Note: Subversion does not offer an XML output option for this subcommand
  *
+ * @category VersionControl
  * @package  VersionControl_SVN
- * @version  @version@
- * @category SCM
  * @author   Clay Loveless <clay@killersoft.com>
+ * @author   Michiel Rook <mrook@php.net>
+ * @license  http://www.killersoft.com/LICENSE.txt BSD License
+ * @version  @version@
+ * @link     http://pear.php.net/package/VersionControl_SVN
  */
 class VersionControl_SVN_Add extends VersionControl_SVN
 {
@@ -134,9 +139,8 @@ class VersionControl_SVN_Add extends VersionControl_SVN
      * Valid switches for svn add
      *
      * @var     array
-     * @access  public
      */
-    var $valid_switches = array('q',
+    public $valid_switches = array('q',
                                 'quiet',
                                 'N',
                                 'targets',
@@ -156,34 +160,30 @@ class VersionControl_SVN_Add extends VersionControl_SVN
      * <b>outside</b> of those specified in {@link switches}.
      *
      * @var     array
-     * @access  public
      */
-    var $args = array();
+    public $args = array();
     
     /**
      * Minimum number of args required by this subcommand.
      * See {@link http://svnbook.red-bean.com/svnbook/ Version Control with Subversion}, 
      * Subversion Complete Reference for details on arguments for this subcommand.
      * @var     int
-     * @access  public
      */
-    var $min_args = 1;
+    public $min_args = 1;
     
     /**
      * Switches required by this subcommand.
      * See {@link http://svnbook.red-bean.com/svnbook/ Version Control with Subversion}, 
      * Subversion Complete Reference for details on arguments for this subcommand.
      * @var     array
-     * @access  public
      */
-    var $required_switches = array();
+    public $required_switches = array();
 
     /**
      * Use exec or passthru to get results from command.
      * @var     bool
-     * @access  public
      */
-    var $passthru = false;
+    public $passthru = false;
     
     /**
      * Prepare the svn subcommand switches.
@@ -192,11 +192,10 @@ class VersionControl_SVN_Add extends VersionControl_SVN
      * --xml switch if $fetchmode is set to SVN_FETCHMODE_XML,
      * SVN_FETCHMODE_ASSOC or SVN_FETCHMODE_OBJECT
      *
-     * @param   void
-     * @return  int    true on success, false on failure. Check PEAR_ErrorStack
+     * @return boolean true on success, false on failure. Check PEAR_ErrorStack
      *                 for error details, if any.
      */
-    function prepare()
+    public function prepare()
     {
         $meets_requirements = $this->checkCommandRequirements();
         if (!$meets_requirements) {
@@ -214,31 +213,31 @@ class VersionControl_SVN_Add extends VersionControl_SVN
             if (in_array($switch, $valid_switches)) {
                 $switch = str_replace('_', '-', $switch);
                 switch ($switch) {
-                    case 'targets':
-                    case 'config-dir':
-                        $_switches .= "--$switch $val ";
-                        break;
-                    case 'non-recursive':
-                    case 'auto-props':
-                    case 'no-auto-props':
-                        if ($val === true) {
-                            $_switches .= "--$switch ";
-                        }
-                        break;
-                    case 'quiet':
-                        if ($val === true) {
-                            $_switches .= "--$switch ";
-                        }
-                        break;
-                    case 'q':
-                    case 'N':
-                        if ($val === true) {
-                            $_switches .= "-$switch ";
-                        }
-                        break;
-                    default:
-                        // that's all, folks!
-                        break;
+                case 'targets':
+                case 'config-dir':
+                    $_switches .= "--$switch $val ";
+                    break;
+                case 'non-recursive':
+                case 'auto-props':
+                case 'no-auto-props':
+                    if ($val === true) {
+                        $_switches .= "--$switch ";
+                    }
+                    break;
+                case 'quiet':
+                    if ($val === true) {
+                        $_switches .= "--$switch ";
+                    }
+                    break;
+                case 'q':
+                case 'N':
+                    if ($val === true) {
+                        $_switches .= "-$switch ";
+                    }
+                    break;
+                default:
+                    // that's all, folks!
+                    break;
                 }
             } else {
                 $invalid_switches[] = $switch;
@@ -262,14 +261,21 @@ class VersionControl_SVN_Add extends VersionControl_SVN
         if ($invalid > 0) {
             $params['was'] = 'was';
             $params['is_invalid_switch'] = 'is an invalid switch';
+            
             if ($invalid > 1) {
                 $params['was'] = 'were';
                 $params['is_invalid_switch'] = 'are invalid switches';
             }
+            
             $params['list'] = $invalid_switches;
             $params['switches'] = $switches;
             $params['_svn_cmd'] = ucfirst($this->_svn_cmd);
-            $this->_stack->push(VERSIONCONTROL_SVN_NOTICE_INVALID_SWITCH, 'notice', $params);
+            
+            $this->_stack->push(
+                VERSIONCONTROL_SVN_NOTICE_INVALID_SWITCH,
+                'notice',
+                $params
+            );
         }
         return true;
     }
@@ -280,42 +286,45 @@ class VersionControl_SVN_Add extends VersionControl_SVN
     /**
      * Handles output parsing of output of command.
      *
-     * @param   array   $out    Array of output captured by exec command in {@link run}.
-     * @return  mixed   Returns output requested by fetchmode (if available), or raw output
-     *                  if desired fetchmode is not available.
-     * @access  public
+     * @param array $out Array of output captured by exec command in {@link run}.
+     *
+     * @return mixed Returns output requested by fetchmode (if available),
+     *               or raw output if desired fetchmode is not available.
      */
-    function parseOutput($out)
+    public function parseOutput($out)
     {
         $fetchmode = $this->fetchmode;
         switch($fetchmode) {
-            case VERSIONCONTROL_SVN_FETCHMODE_RAW:
-                return join("\n", $out);
-                break;
-            case VERSIONCONTROL_SVN_FETCHMODE_ASSOC:
-                // Temporary, see parseOutputArray below
-                return join("\n", $out);
-                break;
-            case VERSIONCONTROL_SVN_FETCHMODE_OBJECT:
-                // Temporary, will return object-ified array from
-                // parseOutputArray
-                return join("\n", $out);
-                break;
-            case VERSIONCONTROL_SVN_FETCHMODE_XML:
-                // Temporary, will eventually build an XML string
-                // with XML_Util or XML_Tree
-                return join("\n", $out);
-                break;
-            default:
-                // What you get with VERSIONCONTROL_SVN_FETCHMODE_DEFAULT
-                return join("\n", $out);
-                break;
+        case VERSIONCONTROL_SVN_FETCHMODE_RAW:
+            return join("\n", $out);
+            break;
+        case VERSIONCONTROL_SVN_FETCHMODE_ASSOC:
+            // Temporary, see parseOutputArray below
+            return join("\n", $out);
+            break;
+        case VERSIONCONTROL_SVN_FETCHMODE_OBJECT:
+            // Temporary, will return object-ified array from
+            // parseOutputArray
+            return join("\n", $out);
+            break;
+        case VERSIONCONTROL_SVN_FETCHMODE_XML:
+            // Temporary, will eventually build an XML string
+            // with XML_Util or XML_Tree
+            return join("\n", $out);
+            break;
+        default:
+            // What you get with VERSIONCONTROL_SVN_FETCHMODE_DEFAULT
+            return join("\n", $out);
+            break;
         }
     }
     
     /**
      * Helper method for parseOutput that parses output into an associative array
      *
+     * @param array $out Array of output captured by exec command in {@link run}.
+     *
+     * @return mixed
      * @todo Covert list output parsing used in example_tree.php to this method
      *       (more or less)
      */
@@ -324,8 +333,9 @@ class VersionControl_SVN_Add extends VersionControl_SVN
         $parsed = array();
         
         // check switches for verbose output
-        if ((isset($this->switches['v']) && $this->switches['v'] === true) ||
-            (isset($this->switches['verbose']) && $this->switches['verbose'] === true)) {
+        if ((isset($this->switches['v']) && $this->switches['v'] === true) 
+            || (isset($this->switches['verbose']) && $this->switches['verbose'] === true)
+        ) {
             
             
         } else {
